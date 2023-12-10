ee46493095d4e81bc3a27f4fdcbb4942a536b0a7
doneni <xxerrenn@icloud.com>
2023-12-10 16:13:57 +0900

    check flag feature done

--- src/services/challenge.service.ts
+++ src/services/challenge.service.ts
@@ -7,7 +7,6 @@ const API_URL = import.meta.env.VITE_BACKEND_API_URL
 class ChallengeService {
   async getAllChallenges(): Promise<Challenges> {
     const response = await axios.get(API_URL + 'challenge/get-all-challenges')
-    console.log(response.data.challenge)
     return response.data
   }
 
@@ -16,24 +15,28 @@ class ChallengeService {
       params: { layer, region },
     })
 
+    console.log(response.data)
+
     return response.data
   }
 
-  async checkFlag(title: string, user_flag: string): Promise<User> {
-    const response = await axios.post(API_URL + 'challenge/check-flag', { title, user_flag })
-    return response.data
+  async checkFlag(title: string, user_flag: string): Promise<boolean> {
+    const response = await axios.post(API_URL + 'challenge/check-flag', {
+      title, user_flag,
+    });
+    
+    console.log(response.data)
+
+    return response.data.correct;
   }
+
+  // async checkFlag(title: string, user_flag: string): Promise<User> {
+  //   const response = await axios.post(API_URL + 'challenge/check-flag', {
+  //     title, user_flag,
+  //   });
+        
+  //   return response.data
+  // }
 }
 
 export default new ChallengeService()
-
-/*
-export interface Challenge {
-  id: string
-  title: string
-  region: string
-  layer: string
-  description: string
-  connect: string
-}
-*/
