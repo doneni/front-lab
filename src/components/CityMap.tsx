596c06bb5a4a26af90ebdf1750bf9b9997e08a15
doneni <xxerrenn@icloud.com>
2023-12-15 03:26:01 +0900

    citymap button updated

--- src/components/CityMap.tsx
+++ src/components/CityMap.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Button, Container, Grid, Typography } from '@mui/material'
+import { Button, Container, Grid, } from '@mui/material'
 import { useSnackBar } from '../contexts/snackbar'
 import ChallengeModal from './ChallengeModal'
 import ChallengeListModal from './ChallengeListModal'
@@ -14,23 +14,23 @@ export default function CityMap() {
     '스마트 모빌리티',
     '자율주행 차량',
     '실감 컨텐츠',
-    '스마트 헬스케어',
-    '스마트 팩토리',
-    '스마트 생활',
     '스마트 교육',
+    '스마트 헬스케어',
+    '스마트 생활',
+    '스마트 팩토리',
     '스마트 환경',
   ]
   const location = [
-    ['38vh', '6vw'],
-    ['9vh', '14.5vw'],
-    ['53vh', '18.5vw'],
-    ['42vh', '60vw'],
-    ['17vh', '23vw'],
-    ['40vh', '49vw'],
-    ['58vh', '40vw'],
-    ['60vh', '60vw'],
-    ['48vh', '31.5vw'],
-    ['23vh', '39vw'],
+    ['10vh', '7vw'],
+    ['10vh', '25vw'],
+    ['10vh', '43vw'],
+    ['10vh', '60vw'],
+    ['35vh', '7vw'],
+    ['35vh', '25vw'],
+    ['35vh', '43vw'],
+    ['35vh', '60vw'],
+    ['60vh', '7vw'],
+    ['60vh', '25vw'],
   ]
   const [isChallengeListModalOpen, setIsChallengeListModalOpen] = useState(false)
   const [isEndingModalOpen, setIsEndingModalOpen] = useState(false)
@@ -105,10 +105,25 @@ export default function CityMap() {
     position: 'absolute' as 'absolute',
     backgroundSize: '100% 100%',
     backgroundPosition: 'center',
-    width: '50px',
-    borderRadius: '100%',
+    width: '120px',
+    height: '120px',
     overflow: 'hidden',
     color: 'white',
+    borderRadius: '100%',
+    top,
+    left,
+    backgroundColor,
+  })
+
+  const createChallengeButtonStyle = (top: string, left: string, backgroundColor: string) => ({
+    position: 'absolute' as 'absolute',
+    backgroundSize: '100% 100%',
+    backgroundPosition: 'center',
+    width: '180px',
+    height: '130px',
+    overflow: 'hidden',
+    color: 'white',
+    opacity: 0.8,
     top,
     left,
     backgroundColor,
@@ -116,40 +131,28 @@ export default function CityMap() {
 
   return (
     <main>
-      <Container
-        id='mapContainer'
-        sx={{
-          mt: 2,
-          backgroundImage: `url(./layer/${backgroundImage}.gif)`,
-          backgroundPosition: 'center',
-          height: '85vh',
-          position: 'relative',
-          backgroundRepeat: 'no-repeat',
-          overflow: 'hidden',
-        }}
-      >
-        <Button onClick={() => openChallengeListModal()} style={createButtonStyle('2vh', '2vw', '#4F100C')}>
-          <img
-            src='/map.png'
-            alt='map'
-            style={{ width: '50px', height: '50px', objectFit: 'cover' }}
-          />
-        </Button>
-        <Button onClick={() => setShowRegion(!showRegion)} style={createButtonStyle('12vh', '2vw', '#e3a8a8')}>
-          <img
-            src={`./eye-${!showRegion}.png`}
-            alt='visibility'
-            style={{ width: '50px', height: '50px', objectFit: 'cover' }}
-          />
-        </Button>
-        <Button onClick={() => openEndingModal()} style={createButtonStyle('2vh', '72vw', '#4F100C')}>
-          <img
-            src='/magic-ball.png'
-            alt='magicball'
-            style={{ width: '50px', height: '50px', objectFit: 'cover' }}
-          />
-        </Button>
-        <Grid
+      <Button onClick={() => openChallengeListModal()} style={createButtonStyle('11vh', '1.2vw', '#4F100C')}>
+        <img
+          src='/map.png'
+          alt='map'
+          style={{ width: '90px', height: '90px', objectFit: 'cover' }}
+        />
+      </Button>
+      <Button onClick={() => setShowRegion(!showRegion)} style={createButtonStyle('30vh', '1.2vw', '#e3a8a8')}>
+        <img
+          src={`./eye-${!showRegion}.png`}
+          alt='visibility'
+          style={{ width: '90px', height: '90px', objectFit: 'cover' }}
+        />
+      </Button>
+      <Button onClick={() => openEndingModal()} style={createButtonStyle('11vh', '89.7vw', '#4F100C')}>
+        <img
+          src='/magic-ball.png'
+          alt='magicball'
+          style={{ width: '90px', height: '90px', objectFit: 'cover' }}
+        />
+      </Button>
+      <Grid
           spacing={0}
           direction='column'
           alignItems='center'
@@ -157,10 +160,12 @@ export default function CityMap() {
           style={{
             backgroundColor: 'white',
             minHeight: '10vh',
-            maxWidth: '10vw',
+            maxWidth: '5vw',
             position: 'absolute',
-            top: '62vh',
-            left: '1.5vw',
+            top: '64vh',
+            left: '0.2vw',
+            zIndex: 1,
+            borderInline: 'true',
           }}
         >
           {layers.map((layer) => (
@@ -171,20 +176,36 @@ export default function CityMap() {
                 borderRadius: 0,
                 backgroundColor: selectedLayer === layer ? 'grey' : 'white',
                 color: selectedLayer === layer ? 'black' : 'black',
-                fontSize: '14px',
+                fontSize: '20px',
                 width: '10vw',
+                height: '8vh',
+                border: '1px solid lightGrey',
               }}
             >
               <b>{layer}</b>
             </Button>
           ))}
-        </Grid>
+      </Grid>
+
+      <Container
+        id='mapContainer'
+        sx={{
+          mt: 2,
+          backgroundImage: `url(./layer/${backgroundImage}.gif)`,
+          backgroundPosition: 'center',
+          width: '90vw',
+          height: '85vh',
+          position: 'relative',
+          backgroundRepeat: 'no-repeat',
+          overflow: 'hidden',
+        }}
+      >
 
         {regions.map((region, index) => (
           <Grid item key={region}>
             <Button
               onClick={() => openChallengeModal(selectedLayer, region)}
-              style={createButtonStyle(location[index][0], location[index][1], '#88304A')}
+              style={createChallengeButtonStyle(location[index][0], location[index][1], '#c96381')}
             >
               <img
                 src={`region/${region}.png`}
@@ -205,7 +226,7 @@ export default function CityMap() {
                 overflow: 'hidden',
                 backgroundColor: 'white',
                 opacity: 0.8,
-                transform: 'translate(-30%, 0%)',
+                transform: 'translate(0%, 0%)',
                 display: 'flex',
                 borderRadius: '10%',
                 alignItems: 'center',
